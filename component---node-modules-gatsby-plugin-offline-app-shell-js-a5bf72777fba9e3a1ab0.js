36d10d972002cb683d168e0aba87a0621f17c0ce
Angel Meza <undead.85@gmail.com>
2019-09-27 16:38:20 -0300

    Updates

---
+++ component---node-modules-gatsby-plugin-offline-app-shell-js-a5bf72777fba9e3a1ab0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{208:function(e,t,n){"use strict";var u=n(18);t.__esModule=!0,t.default=void 0;var o=u(n(75)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-a5bf72777fba9e3a1ab0.js.map
---
+++ component---src-pages-page-2-js-f9de32e6a5f3ffe4bcac.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{210:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(86),l=t(207);n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-f9de32e6a5f3ffe4bcac.js.map
